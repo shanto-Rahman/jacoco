2054d33092efcc7c32e0462cfa02f3e53d6289d2
Shanto Rahman <shanto@ECE-A55006.austin.utexas.edu>
2022-03-30 13:31:04 -0500

    updated Report generator

--- org.jacoco.extract_covered_line/src/org/jacoco/extract_covered_line/ReportGenerator.java
+++ org.jacoco.extract_covered_line/src/org/jacoco/extract_covered_line/ReportGenerator.java
@@ -43,153 +43,121 @@ import org.jacoco.core.analysis.IMethodCoverage;
  */
 public class ReportGenerator {
 
-	private final String title;
+    private final String title;
     private String destinationDirectory;
-	private final File executionDataFile;
-	private final File classesDirectory;
-	private final File sourceDirectory;
-	private final File reportDirectory;
+    private final File executionDataFile;
+    private final File classesDirectory;
 
-	private ExecFileLoader execFileLoader;
+    private ExecFileLoader execFileLoader;
 
-	/**
-	 * Create a new generator based for the given project.
-	 *
-	 * @param projectDirectory
-	 */
-	public ReportGenerator(final File projectDirectory) {
+    /**
+     * Create a new generator based for the given project.
+     *
+     * @param projectDirectory
+     */
+    public ReportGenerator(final File projectDirectory) {
         System.out.println( projectDirectory );
         //Paths path = Paths.get(projectDirectory).
         this.destinationDirectory = projectDirectory.getAbsolutePath();
-		this.title = projectDirectory.getName();
-		this.executionDataFile = new File(projectDirectory, "jacoco.exec");
-		this.classesDirectory = new File(projectDirectory, "classes");
-		this.sourceDirectory = new File(projectDirectory, "src");
-		this.reportDirectory = new File(projectDirectory, "coveragereport");
-	}
+        this.title = projectDirectory.getName();
+        this.executionDataFile = new File(projectDirectory, "jacoco.exec");
+        this.classesDirectory = new File(projectDirectory, "classes");
+    }
 
-	/**
-	 * Create the report.
-	 *
-	 * @throws IOException
-	 */
-	public void create() throws IOException {
+    /**
+     * Create the report.
+     *
+     * @throws IOException
+     */
+    public void create() throws IOException {
 
-		// Read the jacoco.exec file. Multiple data files could be merged
-		// at this point
-		loadExecutionData();
+        // Read the jacoco.exec file. Multiple data files could be merged
+        // at this point
+        loadExecutionData();
 
-		// Run the structure analyzer on a single class folder to build up
-		// the coverage model. The process would be similar if your classes
-		// were in a jar file. Typically you would create a bundle for each
-		// class folder and each jar you want in your report. If you have
-		// more than one bundle you will need to add a grouping node to your
-		// report
-		final IBundleCoverage bundleCoverage = analyzeStructure();
+        // Run the structure analyzer on a single class folder to build up
+        // the coverage model. The process would be similar if your classes
+        // were in a jar file. Typically you would create a bundle for each
+        // class folder and each jar you want in your report. If you have
+        // more than one bundle you will need to add a grouping node to your
+        // report
+        final IBundleCoverage bundleCoverage = analyzeStructure();
 
-		createReport(bundleCoverage);
+        myReport(bundleCoverage);
+    }
 
-		myReport(bundleCoverage);
-	}
+    public void myReport(final IBundleCoverage bundleCoverage) throws IOException {
+        Collection<IPackageCoverage> packageCollection = bundleCoverage
+                .getPackages();
+        List<String> resultList = new ArrayList<String>();
+        // for(int i= 0; i< packageCol.size(); i++){
+        // System.out.println("Bundle Package ="+ packageCol.get(i));
+        // }
 
-	public void myReport(final IBundleCoverage bundleCoverage) throws IOException {
-		Collection<IPackageCoverage> packageCollection = bundleCoverage
-				.getPackages();
-		List<String> resultList = new ArrayList<String>();
-		// for(int i= 0; i< packageCol.size(); i++){
-		// System.out.println("Bundle Package ="+ packageCol.get(i));
-		// }
-
-		for (IPackageCoverage packageName : packageCollection) {
-			// System.out.println(packageName);
-			Collection<IClassCoverage> classCollection = packageName
-					.getClasses();
-			for (IClassCoverage className : classCollection) {
-				 //System.out.println(className.getName());
-				//String Str = new String("org/java_websocket/SocketChannelIOHelper");
-				for (int i = className.getFirstLine(); i <= className.getLastLine(); i++) {
-					final int status = className.getLine(i).getStatus();
-					// className.getLine(i).getStatus()
-					switch (status) {
-					case ICounter.NOT_COVERED:
-						break;
-					case ICounter.PARTLY_COVERED:
-						resultList.add(className.getName() + ":" + Integer.valueOf(i));
-						break;
-					case ICounter.FULLY_COVERED:
-						resultList.add(className.getName() + ":" + Integer.valueOf(i));
-						break;
-					}
-				}
-			}
-		}
-		System.out.println("List Size =" + resultList.size());
+        for (IPackageCoverage packageName : packageCollection) {
+            // System.out.println(packageName);
+            Collection<IClassCoverage> classCollection = packageName
+                    .getClasses();
+            for (IClassCoverage className : classCollection) {
+                 //System.out.println(className.getName());
+                //String Str = new String("org/java_websocket/SocketChannelIOHelper");
+                for (int i = className.getFirstLine(); i <= className.getLastLine(); i++) {
+                    final int status = className.getLine(i).getStatus();
+                    // className.getLine(i).getStatus()
+                    switch (status) {
+                    case ICounter.NOT_COVERED:
+                        break;
+                    case ICounter.PARTLY_COVERED:
+                        resultList.add(className.getName() + ":" + Integer.valueOf(i));
+                        break;
+                    case ICounter.FULLY_COVERED:
+                        resultList.add(className.getName() + ":" + Integer.valueOf(i));
+                        break;
+                    }
+                }
+            }
+        }
+        System.out.println("List Size =" + resultList.size());
         
-		FileWriter writer = new FileWriter(destinationDirectory +"/output.csv");
-		String collect = resultList.stream().collect(Collectors.joining(","));
-		writer.write(collect);
-		writer.close();
+        FileWriter writer = new FileWriter(destinationDirectory +"/output.csv");
+        String collect = resultList.stream().collect(Collectors.joining(","));
+        writer.write(collect);
+        writer.close();
 
-	}
+    }
 
-	private void createReport(final IBundleCoverage bundleCoverage)
-			throws IOException {
+    private void loadExecutionData() throws IOException {
+        execFileLoader = new ExecFileLoader();
+        execFileLoader.load(executionDataFile);
+    }
 
-		// Create a concrete report visitor based on some supplied
-		// configuration. In this case we use the defaults
-		final HTMLFormatter htmlFormatter = new HTMLFormatter();
-		final IReportVisitor visitor = htmlFormatter
-				.createVisitor(new FileMultiReportOutput(reportDirectory));
+    private IBundleCoverage analyzeStructure() throws IOException {
+        final CoverageBuilder coverageBuilder = new CoverageBuilder();
+        final Analyzer analyzer = new Analyzer(
+                execFileLoader.getExecutionDataStore(), coverageBuilder);
 
-		// Initialize the report with all of the execution and session
-		// information. At this point the report doesn't know about the
-		// structure of the report being created
-		visitor.visitInfo(execFileLoader.getSessionInfoStore().getInfos(),
-				execFileLoader.getExecutionDataStore().getContents());
+        analyzer.analyzeAll(classesDirectory);
 
-		// Populate the report structure with the bundle coverage information.
-		// Call visitGroup if you need groups in your report.
-		visitor.visitBundle(bundleCoverage,
-				new DirectorySourceFileLocator(sourceDirectory, "utf-8", 4));
+        return coverageBuilder.getBundle(title);
+    }
 
-		// Signal end of structure information to allow report to write all
-		// information out
-		visitor.visitEnd();
-
-	}
-
-	private void loadExecutionData() throws IOException {
-		execFileLoader = new ExecFileLoader();
-		execFileLoader.load(executionDataFile);
-	}
-
-	private IBundleCoverage analyzeStructure() throws IOException {
-		final CoverageBuilder coverageBuilder = new CoverageBuilder();
-		final Analyzer analyzer = new Analyzer(
-				execFileLoader.getExecutionDataStore(), coverageBuilder);
-
-		analyzer.analyzeAll(classesDirectory);
-
-		return coverageBuilder.getBundle(title);
-	}
-
-	/**
-	 * Starts the report generation process
-	 *
-	 * @param args
-	 *            Arguments to the application. This will be the location of the
-	 *            eclipse projects that will be used to generate reports for
-	 * @throws IOException
-	 */
-	public static void main(final String[] args) throws IOException {
-		
+    /**
+     * Starts the report generation process
+     *
+     * @param args
+     *            Arguments to the application. This will be the location of the
+     *            eclipse projects that will be used to generate reports for
+     * @throws IOException
+     */
+    public static void main(final String[] args) throws IOException {
+        
         for (int i = 0; i < args.length; i++) {
-			System.out.println("HELLO SHANTO");
-			final ReportGenerator generator = new ReportGenerator(new File(args[i]));
+            System.out.println("HELLO SHANTO");
+            final ReportGenerator generator = new ReportGenerator(new File(args[i]));
             
-			System.out.println(args[i]);
-			generator.create();
-		}
-	}
+            System.out.println(args[i]);
+            generator.create();
+        }
+    }
 
 }
